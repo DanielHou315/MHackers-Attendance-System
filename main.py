6787e115a06aea9ce3e1a6b6e136c363fe894e32
Daniel Hou <danielhou315@gmail.com>
2023-04-08 11:40:58 -0400

    Created config files

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+
+'''
+
+There are a few things this script does: 
+
+1. Create a google form and a google sheet
+2. Aggregate the google form 
+
+Planned Future Support 
+1. Custom Naming Scheme for form and url link
+'''
+
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
+
+
+
