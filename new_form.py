c6c436eee31a84cd91574ca188ec4bdf1d920c86
Daniel Hou <danielhou315@gmail.com>
2023-04-08 14:30:59 -0400

    Updated new_form to include basic google api

--- new_form.py
+++ new_form.py
@@ -1,6 +1,14 @@
 import requests
 import pyshorteners
 from datetime import datetime
+
+
+import os.path
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 '''
 
 There are a few things this script does: 
@@ -16,7 +24,56 @@ SHORT_URL = "NULL"
 LONG_URL = "NULL"
 
 
+def new_form_sheet():
+    dateobj = datetime.today()
+    aliasing = "mhackers{0}{1}{2}".format(dateobj.year, dateobj.month, dateobj.day)
+    # Questions to include in the form/sheet
 
+    questions = [
+        "Your Name",
+        "Your Uniqname",
+        "Anything you'd like us to know?",
+    ]
+
+    # Google Sheets API setup
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+    creds = None
+    if os.path.exists('credentials.json'):
+        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    elif os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            raise Exception('Unable to obtain valid Google Sheets API credentials.')
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Create a new Google Sheet
+    spreadsheet = {
+        'properties': {
+            'title': f"MHackers-Attendance-{dateobj.year}={dateobj.month}-{dateobj.day}"
+        }
+    }
+    sheet = service.spreadsheets().create(body=spreadsheet, fields='spreadsheetId').execute()
+    sheet_id = sheet.get('spreadsheetId')
+    print(f'Created new Google Sheet with ID: {sheet_id}')
+
+    # Add questions to the Google Sheet
+    range_ = 'Sheet1!A1:A' + str(len(questions))
+    body = {
+        'range': range_,
+        'majorDimension': 'ROWS',
+        'values': [[q] for q in questions]
+    }
+    result = service.spreadsheets().values().update(
+        spreadsheetId=sheet_id, range=range_, valueInputOption='RAW', body=body).execute()
+    print(f'{result.get("updatedCells")} cells updated.')
 
 
 def print_obvious(content):
